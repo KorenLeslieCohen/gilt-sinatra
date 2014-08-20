83a2547ddfa5a3042706c265b820e2df486520e2
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-19 22:58:21 -0400

    random affirmations working

--- public/products.js
+++ public/products.js
@@ -9,19 +9,12 @@ $(document).ready(function(){
         type: 'GET',
         dataType: 'json',
         success: function (response) {
-
             console.log("Gilt API successful");
-            // console.log(response.sales);
-            // console.log("Response length: " + response.sales.length);
-            // for (var i = 0; i < response.sales.length; i++) {
 
             for (var i = 0; i < 10; i++) {
               var sale_name = response.sales[i].name;
               var sale_description = response.sales[i].description;
               var sale_url = response.sales[i].sale_url;
-  
-              // console.log(sale_name);
-              // console.log(sale_url);
 
               for (var key in response.sales[i].image_urls) {
                   var image_object = response.sales[i].image_urls[key];
@@ -31,26 +24,25 @@ $(document).ready(function(){
                   var image = image_object[key2].url;
                }   
 
-            $('#products').append("<li><a href='" + sale_url + "'target='_blank'><div class='sale_info' style='background-image:url(\"" + image + "\"); background-size:cover'><h1>" + sale_name + "</h1><br><p>" + sale_description + "</p>" + "</div></a></li>");
-            $('#products').fadeIn(3000);
-
+              $('#products').append("<li><a href='" + sale_url + "'target='_blank'><div class='sale_info' style='background-image:url(\"" + image + "\"); background-size:cover'><h1>" + sale_name + "</h1><br><p>" + sale_description + "</p>" + "</div></a></li>");
+              $('#products').fadeIn(3000);
 
           }
 
-          $('li').mouseenter(function(){
-            $(this).closest('li').find('h1').append("<h2>You know you want these.</h2>");
-              $('li').mouseleave(function(){
-                $('h2').remove();
+            $('li').mouseenter(function(){
+              var affirmationArray = ['You know you want these.', 'You look fantastic today.', 'Treat yourself.', 'Buy yourself a present.'];
+              var rand = affirmationArray[Math.floor(Math.random() * affirmationArray.length)];
+              $(this).closest('li').find('h1').append("<h2>" + rand + "</h2>");
+                $('li').mouseleave(function(){
+                  $('h2').remove();
+              });
             });
-          });
+
         },
         error: function (response) {
             console.log("Gilt API request failed");
         }
 
-
     })
-
-
 
 });
